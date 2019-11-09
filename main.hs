9bf4bf081f5012c843b128d57d68b2df9d9723db
Daniel Moesgaard <moesgaarda@gmail.com>
2019-11-09 13:44:12 +0100

    Added creation of the tree

--- main.hs
+++ main.hs
@@ -1,21 +1,54 @@
+import Data.List (sortOn)
+
 type Symbol = Char
 type Weight = Int
 
-count :: [Symbol] -> [(Symbol, Weight)]
+data Tree = Leaf Char Int
+            | Branch Tree Tree [Symbol] Weight
+                deriving (Show)
+            -- Subtree has a [Symbol] so that we can
+            -- Do a full pattern matching in getSymbol,
+            -- However, it'll always be an empty [].
+
+count :: [Symbol] -> [Tree]
 count [] = []
-count inputString = [(x, y) | x<-['0'..'z'], let y = (length.filter (==x)) inputString, y>0]
+count inputString = makeLeaf (sortOn snd [(x, y) | x<-[' '..'z'], let y = (length.filter (==x)) inputString, y>0])
+    -- sortOn fra data.List hvor snd er andet element i tuplen
+    -- x er en liste af alle bogstaver den skal genkende (0-9, A-z)
+    -- for hvert bogstav, tjek hvor mange gange det opstår
+    -- Filtrer alle der ikke er der mindst 1 gang
 
--- x er en liste af alle bogstaver den skal genkende (0-9, A-z)
--- for hvert bogstav, tjek hvor mange gange det opstår
--- Filtrer alle der ikke er der mindst 1 gang
+makeLeaf :: [(Char, Int)] -> [Tree]
+makeLeaf [] = []
+makeLeaf (x:xs) = (Leaf (fst x) (snd x)) : makeLeaf xs
 
-data Node = Leaf Symbol Weight
-            | Subtree Node Node [Symbol] Weight
-
-getWeight :: Node -> Weight
+getWeight :: Tree -> Weight
 getWeight (Leaf _ w) = w
-getWeight (Subtree _ _ _ w) = w
+getWeight (Branch _ _ _ w) = w
 
-getSymbol :: Node -> [Symbol]
+getSymbol :: Tree -> [Symbol]
 getSymbol (Leaf s _) = [s]
-getSymbol (Subtree _ _ s _) = s
+getSymbol (Branch _ _ s _) = s
+
+insertToTree :: Tree -> [Tree] -> [Tree]
+insertToTree n [] = [n]
+insertToTree n (y:ys) =  if (getWeight n) <= (getWeight y)
+                    then n:y:ys
+                    else y: insertToTree n ys
+-- Tjek om vægten på den nuværende er den laveste sæt ind
+-- ellers kør igen på første element i xs som burde være den laveste i listen
+
+-- choose the two with lowest number, merge them to new branch
+-- releat until only one largest?
+
+
+makeTree :: [Tree] -> Tree
+makeTree [n] = n
+makeTree (first:second:rest) =
+    let n = Branch
+                first
+                second
+                ((getSymbol first) ++ (getSymbol second))
+                ((getWeight first) + (getWeight second))
+            in
+        makeTree (insertToTree n rest)
